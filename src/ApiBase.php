30831f2cefeb0139355e2b074bdd707554c05160
zabik <zabik@bnwall.net>
2022-03-21 16:34:18 +0300

    1.0.4

--- src/ApiBase.php
+++ src/ApiBase.php
@@ -41,8 +41,6 @@ abstract class ApiBase
     protected function multipart($url, $filepath, $options)
     {
 
-        print '['.$filepath.']'.PHP_EOL;
-
         if (file_exists($filepath)) {
             $multipart = [
                 'headers' => [
